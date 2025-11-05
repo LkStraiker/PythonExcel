b655b40b3ee490e43a1bde622b85cb2b8d19122c
Luccas Oliveira <luccasadolfoconsultores@gmail.com>
2025-11-05 13:03:13 -0300

    Criação da tabela no PDF

--- main.py
+++ main.py
@@ -25,9 +25,30 @@ def auto_widths(writer, df: pd.DataFrame, sheet_name: str, minw=10, maxw=35):
     for i, col in enumerate(df.columns):
         try:
             series = df[col].astype(str)
-            width = len(series.map(len).max(), len(str(col))) + 2
+            width = max(series.map(len).max(), len(str(col))) + 2
             width = max(minw, min(maxw, width))
             ws.set_column(i, i, width)
+        except Exception:
+            ws.set_column(i, i, minw)
+
+def add_table(pdf,df, title):
+    "Criar a tabela no PDF"
+    fig, ax = plt.subplots(figsize=(11.69,8.27))
+    ax.axis("Off")
+    ax.set_title(title, fontsize=14, fontweight='bold', pad=10)
+    show = df.head(20).copy()
+    tbl = ax.table(cellText=show.values, colLabels=show.columns, loc='center')
+    tbl.auto_set_font_size(False)
+    tbl.set_fontsize(8)
+    tbl.scale(1, 1.2)
+    pdf.savefig(bbox_inches='tight')
+    plt.close(fig)
+
+
+
+
+
+
 
 
 
